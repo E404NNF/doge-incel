c75cf10cf5e2522a2dfdec1c7a140c03fb91f296
E404NNF <e404nnf@gmail.com>
2020-08-20 23:36:05 +0200

    help

--- setup.py
+++ setup.py
@@ -1,28 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='doge',
+    name='doge-incel',
     version='3.6.0',
-<<<<<<< HEAD
-<<<<<<< HEAD
     url='https://github.com/E404NNF/incel-homophobic-doge',
     author='E404NNF',
-    author_email='',
-    description=('wow very terminal incel/homophbic doge'),
-=======
-    url='https://github.com/E404NNF/incel-homophobic-doge',
-    author='E404NNF',
-    author_email='',
-    description=('wow very terminal incel/homophbic doge'),
->>>>>>> parent of 821e6b4... Update setup.py
-=======
-    url='https://github.com/thiderman/doge',
-    author='Lowe Thiderman',
     author_email='lowe.thiderman@gmail.com',
-    description=('wow very terminal doge'),
->>>>>>> parent of 821e6b4... Update setup.py
+    description=('wow very terminal incel/homophbic doge'),
     license='MIT',
-    packages=['doge'],
+    packages=['doge-incel'],
     package_data={'doge': ['static/*.txt']},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -31,5 +17,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2',
     ],
-    entry_points={'console_scripts': ['doge = doge.core:main']},
+    entry_points={'console_scripts': ['doge-incel = doge.core:main']},
 )
+
