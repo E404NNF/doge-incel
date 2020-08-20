3f07c3aad96148c195e063b30c64f25c35890ce0
E404NNF <e404nnf@gmail.com>
2020-08-20 23:32:45 +0200

    Github desktop is being gay with me

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='doge-incel',
+    name='doge',
     version='3.6.0',
+<<<<<<< HEAD
 <<<<<<< HEAD
     url='https://github.com/E404NNF/incel-homophobic-doge',
     author='E404NNF',
@@ -14,9 +15,15 @@ setup(
     author_email='',
     description=('wow very terminal incel/homophbic doge'),
 >>>>>>> parent of 821e6b4... Update setup.py
+=======
+    url='https://github.com/thiderman/doge',
+    author='Lowe Thiderman',
+    author_email='lowe.thiderman@gmail.com',
+    description=('wow very terminal doge'),
+>>>>>>> parent of 821e6b4... Update setup.py
     license='MIT',
-    packages=['doge-incel'],
-    package_data={'doge-incel': ['static/*.txt']},
+    packages=['doge'],
+    package_data={'doge': ['static/*.txt']},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
@@ -24,5 +31,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2',
     ],
-    entry_points={'console_scripts': ['doge-incel = doge.core:main']},
+    entry_points={'console_scripts': ['doge = doge.core:main']},
 )
